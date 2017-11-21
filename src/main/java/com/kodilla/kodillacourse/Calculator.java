db778eb8fe509f00a1b169c73f144c40dcdf31b0
KatarzynaZych <korzonek.katarzyna@gmail.com>
2017-11-21 22:04:50 +0000

    moj kalkulator

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,22 @@
+package com.kodilla.kodillacourse;
+
+ class Calculator {
+
+    int a;
+    int b;
+
+    public String calculator(int a, int b){
+
+        int sum = a + b;
+        int subtraction = a - b;
+
+        return "sum = " + sum + " subtraction = " + subtraction;
+    }
+
+    public static void main(String[] args){
+
+        Calculator result = new Calculator();
+        System.out.println(result.calculator(5,3));
+
+    }
+}
